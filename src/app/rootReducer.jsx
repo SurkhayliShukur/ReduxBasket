ab1e980336fa7ffce10a54a399d88691bfc540ea
Shukur939 <shukursurkhayli000@gmail.com>
2023-12-07 19:50:06 +0400

    FinalThings

--- src/app/rootReducer.jsx
+++ src/app/rootReducer.jsx
@@ -1,10 +1,9 @@
 import {combineReducers} from "@reduxjs/toolkit";
 import  basketSlice  from "../slices/basketSlice";
-import  walletSlice  from "../slices/walletSlices";
+
 
 const rootReducer = combineReducers({
     basket: basketSlice,
-    wallet:walletSlice
 })
 
 export default rootReducer;
